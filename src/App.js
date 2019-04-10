2a3b12f317583a7686a57848c899f417ba9a695e
Amit <amitzmn@gmail.com>
2019-04-10 12:38:09 -0400

    Added GA tracking for React

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 // import './App.css';
 
+import ReactGA from 'react-ga';
+
 import Preloader from './components/preloader/preloader';
 import Header from './components/header/header';
 import Home from './components/home/home';
@@ -13,6 +15,9 @@ import Image from  './components/image/image';
 import Photoswipe from  './components/photoswipe/photoswipe';
 import Contact from './components/contact/contact';
 import Footer from './components/footer/footer';
+
+ReactGA.initialize('UA-101996018-1');
+ReactGA.pageview("/");
 
 class App extends Component {
   render() {
